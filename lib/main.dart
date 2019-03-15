4bb966882602fc189c56428b158ca9b81006f2f2
swe <wendler@uni-potsdam.de>
2019-03-15 16:25:24 +0100

    stateful widget text i/o

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,62 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  final nameController = TextEditingController();
+  final zahlController = TextEditingController();
+  var ausgabe = '';
+
+  changeText(){
+    setState(() {
+      return ausgabe = "Hallo ${nameController.text}, du hast ${zahlController.text} eingegeben";
+    });
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    nameController.dispose();
+    zahlController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Meine Flutter-Test-App'),
+          leading: Icon(Icons.ac_unit),
+          title: Text('This is an AppBar'),
         ),
         body: Center(
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Das ist der Startpunkt'),
-                Text('Nur Scaffold. Alles auf GitHub'),
-              ],
+          child: Column(children: <Widget>[
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: 'Name eingeben',
+              ),
             ),
-          ),
+            TextField(
+              controller: zahlController,
+              keyboardType: TextInputType.numberWithOptions(),
+              decoration: InputDecoration(
+                labelText: 'Zahl eingeben',
+              ),
+            ),
+            Text(ausgabe),
+          ]),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.apps),
+          onPressed: (){
+            changeText();
+          },
         ),
       ),
     );
